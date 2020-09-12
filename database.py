99126920000458301cbea9d1bf04290dd8cbde08
Charles Tilbury <chucktilbury@gmail.com>
2020-09-11 19:06:31 -0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,5 @@
+'''
+This is a stub for the database. It exists to allow the formWidgets to be tested. There is no actual
+database functionality in this file, but it does provide a template for implementing a real database
+that interfaces to the form widgets.
+'''
