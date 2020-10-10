687c034bfdd0b3507ad2759a8f1384adcd9d6a92
Chuck Tilbury <chucktilbury@gmail.com>
2020-10-10 14:47:01 -0500

    bug fixes on the select dialog

--- search_widget.py
+++ search_widget.py
@@ -5,9 +5,17 @@ from tkinter.messagebox import *
 from database import Database
 from form_widgets import toolTip
 
+swtt = \
+'''
+Search for entry:
+Type a few characters and the hit the down arrow
+to see the list of current matches. Choose one and
+select the "OK" button.
+'''
+
 class searchWidget(ttk.Combobox):
 
-    def __init__(self, owner, table, column, tool_tip=None, **kw):
+    def __init__(self, owner, table, column, tool_tip=swtt, **kw):
         super().__init__(owner, **kw)
 
         self.table = table
@@ -15,37 +23,21 @@ class searchWidget(ttk.Combobox):
         if not tool_tip is None:
             self.ttip = toolTip(self, tool_tip)
 
-        self.bind('<Key>', self._key_handler)
+        self.bind('<KeyRelease>', self._key_handler)
         self.data = Database.get_instance()
-
-        self.key = ''
-        self.lst = self._generate_list()
-        #self.lst.sort()
-        self.values = self.lst
-
-        #self.values = ['value1', 'value2', 'value3', 'value4', 'value5']
-
-    def _key_handler(self, event=None):
-        print(event, self.get())
-        # if event.char >= '0' and event.char <= '9' \
-        #         or event.char >= 'a' and event.char <= 'z' \
-        #         or event.char >= 'A' and event.char <= 'Z':
-        #     self.key += event.char
-        # elif event.char == '%':
-        #     self.key += '%'
-        # elif event.char == '\x08':
-        #     self.key = self.key[:-1]
 
         self._generate_list()
 
+    def _key_handler(self, event=None):
+        print(event, self.get())
+        self._generate_list()
+
     def _generate_list(self):
-        #newkey = self.key + '%'
         newkey = self.get() + '%'
         line = 'SELECT %s FROM %s WHERE %s LIKE \'%s\''%(self.column, self.table, self.column, newkey)
 
         cur = self.data.execute(line)
         lst = []
-        #lst.append(self.key)
 
         try:
             for item in cur:
@@ -54,5 +46,7 @@ class searchWidget(ttk.Combobox):
             print('exception')
             pass
 
+        lst.sort()
         print(lst)
         self['values'] = lst
+        #self.set(lst[0])
